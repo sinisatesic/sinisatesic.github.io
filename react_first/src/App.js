4eaccf3968a9e2dee9a72723701c1e6326c64549
sinisatesic <sinisa.tesic210@gmail.com>
2022-08-17 18:12:18 -0500

    app.js react_first

--- react_first/src/App.js
+++ react_first/src/App.js
@@ -8,13 +8,13 @@ import SearchIcon from './search.svg';
 const API_URL = `http://www.omdbapi.com?apikey=6fa300fc`
 
 
-const movie1 = {
-    "Title": "Shrek",
-    "Year": "2001",
-    "imdbID": "tt0126029",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}
+// const movie1 = {
+//     "Title": "Shrek",
+//     "Year": "2001",
+//     "imdbID": "tt0126029",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+// }
 
 
 const App = () => {
