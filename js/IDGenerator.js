e00c58ca3fc2b7e4d7d09a820df0f0ee2abccbaa
sinisatesic <sinisa.tesic210@gmail.com>
2026-06-15 22:25:05 -0500

    modifying js idgenerator

--- js/IDGenerator.js
+++ js/IDGenerator.js
@@ -7,49 +7,37 @@ const input = document.getElementById('input');
 let createdText;
 
 const generateIDs = () => {
-console.log(input.value)
-const randomizer = () => {
-let arr = [];
-// let arr = [], specialChars = '!$#&@';
 
-// arr.push(Math.floor((Math.random() * 9) + 1));
-// arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
-// arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toLowerCase());
-// arr.push(specialChars.charAt(Math.floor(Math.random() * 5)))
-// arr.push(Math.floor((Math.random() * 9) + 1));
-// arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
-// arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toLowerCase());
-// arr.push(specialChars.charAt(Math.floor(Math.random() * 5)))
-// arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
-// arr.push(specialChars.charAt(Math.floor(Math.random() * 5)))
+    console.log(input.value)
 
-// arr.push(Math.floor((Math.random() * 9) + 1));
-// arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
+    const randomizer = () => {
 
+    let arr = [];
 
-// arr.push(Array(4).fill(Math.floor(Math.random() * 9) + 1));
-// arr.push(...Array(3).fill(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase()));
-// arr.push(...Array(4).fill(Math.floor(Math.random() * 9) + 1));
-
-    for(let i = 0; i < 3; i++){
-        arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
+    while (content.includes(createdText)){
+        arr = [];
+        
     }
 
-    for(let i = 0; i < 3; i++){
-        arr.push(Math.floor((Math.random() * 9) + 1));
-    }
+        for(let i = 0; i < 3; i++){
+            arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
+        }
 
-    // console.log(arr.join(','));
-    // content.innerHTML = '';
-    createdText = document.createElement('text');
-    createdText.innerHTML = `${arr.join('')}<br>`;
-    createdText.classList.add('opacityFont');
-    content.appendChild(createdText);
-    }
+        for(let i = 0; i < 4; i++){
+            arr.push(Math.floor((Math.random() * 9) + 1));
+        }
 
-    for(let i = 0; i < input.value; i++){
-    randomizer();
-    }
+        createdText = document.createElement('text');
+        createdText.innerHTML = `${arr.join('')}<br>`;
+        createdText.classList.add('opacityFont');
+
+
+        content.appendChild(createdText);
+        }
+
+        for(let i = 0; i < input.value; i++){
+            randomizer();
+        }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
