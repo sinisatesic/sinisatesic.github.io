9c66b0d55b396d7ec3d123343aa5d97ecacca810
sinisatesic <sinisa.tesic210@gmail.com>
2026-06-15 21:47:46 -0500

    js idgenerator, modified syntax and spacing

--- js/IDGenerator.js
+++ js/IDGenerator.js
@@ -6,11 +6,11 @@ const content = document.getElementById("content");
 const input = document.getElementById("input");
 let createdText;
 
-const logs = () => {
+const generateIDs = () => {
 console.log(input.value)
 const randomizer = () => {
-
-let arr = [], specialChars = '!$#&@';
+let arr = [];
+// let arr = [], specialChars = '!$#&@';
 
 // arr.push(Math.floor((Math.random() * 9) + 1));
 // arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
@@ -31,50 +31,53 @@ let arr = [], specialChars = '!$#&@';
 // arr.push(...Array(3).fill(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase()));
 // arr.push(...Array(4).fill(Math.floor(Math.random() * 9) + 1));
 
-for(let i = 0; i < 3; i++){
-    arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
-}
+    for(let i = 0; i < 3; i++){
+        arr.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase());
+    }
 
-for(let i = 0; i < 3; i++){
-    arr.push(Math.floor((Math.random() * 9) + 1));
-}
+    for(let i = 0; i < 3; i++){
+        arr.push(Math.floor((Math.random() * 9) + 1));
+    }
 
-// console.log(arr.join(','));
-// content.innerHTML = ``;
-createdText = document.createElement('text');
-createdText.innerHTML = `${arr.join('')}<br>`;
-createdText.classList.add('opacityFont');
-content.appendChild(createdText);
-}
+    // console.log(arr.join(','));
+    // content.innerHTML = ``;
+    createdText = document.createElement('text');
+    createdText.innerHTML = `${arr.join('')}<br>`;
+    createdText.classList.add('opacityFont');
+    content.appendChild(createdText);
+    }
 
-for(let i = 0; i < input.value; i++){
-randomizer();
-}
+    for(let i = 0; i < input.value; i++){
+    randomizer();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-generateButton.addEventListener('click', logs);
-generateButton.addEventListener('click', () => {
-initialText.innerHTML = ``;
-content.classList.add('postClickBorder');
-content.classList.add('vertExpand');
-createdText.classList.add('opacityFont');
-copyButton.innerHTML = `Copy All`
+    generateButton.addEventListener('click', generateIDs);
+    generateButton.addEventListener('click', () => {
+    initialText.innerHTML = ``;
+    content.classList.add('postClickBorder');
+    content.classList.add('vertExpand');
+    createdText.classList.add('opacityFont');
+    copyButton.innerHTML = `Copy All`
 })
+
 clearButton.addEventListener('click', () => {
-location.reload();
-copyButton.innerHTML = `Copy All`
+    location.reload();
+    copyButton.innerHTML = `Copy All`;
 })
+
 copyButton.addEventListener('click', copyToClipboard);
+
 copyButton.addEventListener('click', () => {
-copyButton.innerHTML = `Copied`;
-})
+    copyButton.innerHTML = `Copied`;
+    })
 })
 
 document.addEventListener('keypress', (e) => {
-if (13 == e.keyCode){
-generateButton.click();
-}
+    if (13 == e.keyCode){
+        generateButton.click();
+    }
 })
 
 const copyToClipboard = () => {
